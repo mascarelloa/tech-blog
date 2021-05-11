8bdc15affe9f86c32cc172d0c7d2e6bafad83f5d
mascarelloa <amfm0793@gmail.com>
2021-05-11 09:59:05 -0400

    updating server.js

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,65 +1,59 @@
-const router = require("express").Router();
-const { User } = require("../../models");
+const router = require('express').Router();
+const { User } = require('../../models');
 
-router.post("/", async (req, res) => {
+//This will create the user upon sign in on the main page.
+router.post('/', async (req, res) => {
   try {
-    const dbUserData = await User.create({
-      username: req.body.username,
-      password: req.body.password,
-    });
+    const userData = await User.create(req.body);
 
     req.session.save(() => {
-      req.session.loggedIn = true;
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
 
-      res.status(200).json(dbUserData);
+      res.status(200).json(userData);
     });
   } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
+    res.status(400).json(err);
   }
 });
 
-router.post("/login", async (req, res) => {
+//explore this code and break it down more/troubleshoot
+router.post('/login', async (req, res) => {
   try {
-    console.log(req.body);
-    const dbUserData = await User.findOne({
-      where: {
-        username: req.body.username,
-      },
-    });
-    console.log(dbUserData);
+    const userData = await User.findOne({ where: { email: req.body.email } });
 
-    if (!dbUserData) {
+    if (!userData) {
       res
         .status(400)
-        .json({ message: "Incorrect username or password. Please try again!" });
+        .json({ message: 'Incorrect email or password, please try again' });
       return;
     }
 
-    const validPassword = await dbUserData.checkPassword(req.body.password);
+    const validPassword = await userData.checkPassword(req.body.password);
 
     if (!validPassword) {
       res
         .status(400)
-        .json({ message: "Incorrect username or password. Please try again!" });
+        .json({ message: 'Incorrect email or password, please try again' });
       return;
     }
 
     req.session.save(() => {
-      req.session.loggedIn = true;
-
-      res
-        .status(200)
-        .json({ user: dbUserData, message: "You are now logged in!" });
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+      
+      res.json({ user: userData, message: 'You are now logged in!' });
     });
+
   } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
+    res.status(400).json(err);
   }
 });
 
-router.post("/logout", (req, res) => {
-  if (req.session.loggedIn) {
+
+//This will logout the user when routed to logout (logout button clicked)
+router.post('/logout', (req, res) => {
+  if (req.session.logged_in) {
     req.session.destroy(() => {
       res.status(204).end();
     });
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,37 +1,37 @@
 const router = require("express").Router();
-const { Blog } = require("../models");
+const { Book, User } = require("../models");
+const withAuth = require("../utils/auth");
 
+//This is the home/index page route.
 router.get("/", async (req, res) => {
   try {
-    const blogData = await Blog.findAll();
-    const blogs = blogData.map((blog) => blog.get({ plain: true }));
-    res.render("homepage", {
-      blogs,
+    //If the user successfully logs in, then they are redirected to profile page.
+    if (req.session.logged_in) {
+      res.redirect("/profile");
+      return;
+    }
+    //The login.handlebars is rendering on this home/index pag route.
+    res.render("login");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//This will route to the profile page when a user logs in or signs up.
+router.get("/profile", withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ["password"] },
+    });
+    const serializedUser = userData.get({plain:true})
+    //The shelf.handlebars will render into secondary.handlebars.
+    res.render("shelf", {
+      layout: "second.handlebars",
+      logged_in: req.session.logged_in,
+      serializedUser
     });
   } catch (err) {
-    console.log(err);
     res.status(500).json(err);
   }
 });
-
-router.get("/blogpost/:id", async (req, res) => {
-  try {
-    const dbPostData = await Blog.findByPk(req.params.id);
-    const singlePost = dbPostData.get({ plain: true });
-    console.log({ singlePost, loggedIn: req.session.loggedIn });
-    res.render("blogpost", { ...singlePost, loggedIn: req.session.loggedIn });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
-});
-
-router.get("/login", (req, res) => {
-  if (req.session.loggedIn) {
-    res.redirect("/");
-    return;
-  }
-  res.render("login");
-});
-
-module.exports = router;
